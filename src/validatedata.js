e64ec414aeb3fdccbb91194d52c7e03ded8127ab
Abhilash177-eng <61733469+Abhilash177-eng@users.noreply.github.com>
2021-12-16 21:09:39 +0530

    commit

--- src/validatedata.js
+++ src/validatedata.js
@@ -9,11 +9,11 @@ const validation = (values) => {
         console.log("email");
         errors.email = "email is required";
     }
-    if(! /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i.test(values.email)){
+    if (! /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i.test(values.email)) {
         console.log("email1");
         errors.email = "email is invalid";
     }
-    if(!values.password) {
+    if (!values.password) {
         console.log("password");
         errors.password = "Password is required!";
     } else if (values.password < 4) {
